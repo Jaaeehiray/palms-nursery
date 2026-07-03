174a4c693386a25d03782308d83e6ab0d775c085
Jaaeehiray <hirayjaaee@gmail.com>
2026-07-04 00:14:18 +0530

    Use Brevo SMTP

--- palms-backend/controllers/inquiryController.js
+++ palms-backend/controllers/inquiryController.js
@@ -2,7 +2,9 @@ const Inquiry = require("../models/Inquiry");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
